409e6320e6cc6e1dbe468c7a6bd30b4e85129533
Kashyap <khpatel4991@gmail.com>
2018-09-04 00:27:12 -0700

    Add cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,3 +1,32 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-export default () => <div>cards</div>
+import styles from '../css/Cards'
+
+const Cards = ({ cards }) => (
+  <div className={styles.cardList}>
+    {cards.map(card => <Card {...card} key={card.id} />)}
+  </div>
+)
+
+const Card = ({
+  name, maxLevel, rarity, iconUrls
+}) => (
+  <div
+    className={styles.card}
+    style={{ backgroundImage: `url(${iconUrls.medium})` }}
+  >
+    <span className={styles.title}>{name}</span>
+
+    <div className={styles.gradient} />
+    <span className={styles.by}>
+      Rarity: {rarity}, MaxLevel: {maxLevel}
+    </span>
+  </div>
+)
+
+const mapState = ({ cards }) => {
+  const cardList = cards || []
+  return { cards: cardList }
+}
+export default connect(mapState)(Cards)
